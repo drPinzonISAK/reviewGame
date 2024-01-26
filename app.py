3a410513a188068952d2bc7b957a3b8c842c551b
Bernard-LTW <34589202+Bernard-LTW@users.noreply.github.com>
2024-01-26 10:32:02 +0900

    Added queuing system to / endpoint, updated host page to reflect

--- app.py
+++ app.py
@@ -1,28 +1,15 @@
-import csv
+import csv,os
 
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, session,abort
 from flask_socketio import SocketIO, emit
 import random
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+players_queue = []
 
-# warm_up = [
-#     ["Com Sci", 94, False, "Computer science is like magic for machines! The best science"],
-#     ["Socializing with Friends", 3, False, "I'll really miss seeing my friends every day and just hanging out with them"],
-#     ["Summer Break", 2, False, "I'll miss the super long break we get during summer. No more worry about school stuff for a while!"],
-#     ["Lunchtime Chats:", 1, False, "Lunch breaks with my friends are the best. We eat together, chat, and have a great time"]
-# ]
-# reasons_for_academic_integrity = [
-#     ["Actually Learning Stuff", 28, False, "Ever felt that \"aha\" moment? Upholding integrity in your studies brings about those lightbulb moments, leading to genuine understanding."],
-#     ["Being True to You", 16, False, "It's about supporting yourself and staying true to your values. Demonstrating integrity through honesty in your work showcases your commitment to ethical principles."],
-#     ["Winning in the Long Run", 14, False, "Think marathon, not sprint. Upholding integrity helps you develop skills that lead to winning in the long haul."],
-#     ["Getting Ready for the Real World", 15, False, "High school is like training for the big leagues (college or whatever's next for you). Integrity now sets you up for success later."],
-#     ["Building Your Character", 9, False, "Picture it as leveling up in the journey of life. Practicing integrity by being honest portrays you as someone with strong principles and a sense of responsibility"],
-#     ["Playing Fair for All", 7, False, "Imagine a fair game where everyone has the same shot. Upholding integrity ensures that nobody's trying to bend the rules."],
-#     ["Trust Rules", 6, False, "Maintaining integrity fosters trust with your teachers and friends. Trust acts as the magical ingredient that enhances relationships in wonderful ways."],
-#     ["Being a Cool Citizen", 5, False, "In a world full of choices, choosing Integrity shows you're a responsible and awesome member of society."]
-# ]
+def is_local_request():
+    return request.remote_addr in ['127.0.0.1', 'localhost']
 def load_csv(file_path):
     data_list = []
     with open(file_path, 'r', newline='', encoding='utf-8') as csvfile:
@@ -40,25 +27,40 @@ def index():
 
 @app.route('/reset_score')
 def reset_score():
-    global score
-    score = [['A', 0], ['B', 0], ['C', 0]]
-    return redirect(url_for('host',question=1))
+    if is_local_request():
+        global score
+        score = [['Tuv', 0], ['KAl', 0], ['Sal', 0], ['Bos', 0], ['Aie', 0], ['Maz', 0]]
+        return redirect(url_for('host', question=1))
+    else:
+        abort(403)
+
 
 @app.route('/host/<int:question>')
 def host(question):
-    global score
-    index = (question - 1) * 2
-    question = data[index][0]
-    options = [[d, 10*len(data[index])-10*i] for i,d in enumerate(data[index][1:])]
-    return render_template('host.html', question=question, options=options,teams=score)
+    if is_local_request():
+        global score
+        index = (question - 1) * 2
+        question = data[index][0]
+        options = [[d, 10*len(data[index])-10*i] for i,d in enumerate(data[index][1:])]
+        return render_template('host2.html', question=question, options=options,teams=score, queue=players_queue)
+    else:
+        abort(403)
+
 
 @app.route('/player/<int:question>')
 def player(question:int):
-    global options, score
-    index = (question - 1) * 2
-    question = data[index][0]
-    options = [[d, False, 10*len(data[index])-10*i] for i,d in enumerate(data[index][1:])]
-    return render_template('player.html', question=question, options=options, teams=score)
+    if is_local_request():
+        global options, score
+        index = (question - 1) * 2
+        question = data[index][0]
+        options = [[d, False, 10*len(data[index])-10*i] for i,d in enumerate(data[index][1:])]
+        return render_template('player.html', question=question, options=options, teams=score)
+    else:
+        abort(403)
+@app.route('/')
+def student():
+    return render_template('index.html')
+
 
 @socketio.on('reveal')
 def reveal(data):
@@ -86,11 +88,24 @@ def addScore(data):
     print(score)
     emit('updateScore',  {'data': score}, broadcast=True)
 
-@socketio.on('addScore_warmup')
-def addScore_warmup(data):
-    global score_warmup
-    score_warmup[data['team']] += data['score']
-    emit('updateScore_warmup',  {'score': score_warmup}, broadcast=True)
+# @socketio.on('addScore_warmup')
+# def addScore_warmup(data):
+#     global score_warmup
+#     score_warmup[data['team']] += data['score']
+#     emit('updateScore_warmup',  {'score': score_warmup}, broadcast=True)
+
+@socketio.on('raise_hand')
+def handle_raise_hand(json):
+    player_name = json['name']
+    if player_name not in players_queue:
+        players_queue.append(player_name)
+        emit('update_queue', {'queue': players_queue}, broadcast=True)
+
+@socketio.on('clear_queue')
+def handle_clear_queue():
+    global players_queue
+    players_queue.clear()
+    emit('update_queue', {'queue': players_queue}, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True,allow_unsafe_werkzeug=True, host='0.0.0.0',port=2345)
